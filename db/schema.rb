63c8ca9ec84ab58d35834faf324ceee63f30a2c1
Onur <onur@deneloper.com>
2025-04-26 09:43:09 +0300

    update connections

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_25_222306) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-  enable_extension "timescaledb"
+  enable_extension "pg_catalog.plpgsql"
 
 end
